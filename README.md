20295acd59b45e8fa1a55608aaed0ed2a5914b66
snowdream <sn0wdr1am@icloud.com>
2020-09-23 14:56:50 +0800

    Update: README.md

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # paginator
+[![PkgGoDev](https://pkg.go.dev/badge/snowdreamtech/paginator)](https://pkg.go.dev/snowdreamtech/paginator)
 
 A paginator for orm(gorm)
-
 ## License
 
 (The MIT License)
 
 Copyright (c) 2019-present SnowdreamTech Inc.
+
--- gorm/gorm.go
+++ gorm/gorm.go
@@ -2,15 +2,23 @@ package gorm
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/snowdreamtech/paginator"
 )
 
 type gormDB struct {
 	*gorm.DB
 }
 
-func (db *gormDB) Paginate(page int, limit int) *gorm.DB {
+func (gormDB *gormDB) Paginate(page int, limit int) *gorm.DB {
+	db := gormDB.DB
 
 	offset := (page - 1) * limit
 
-	return db.DB.Offset(offset).Limit(limit)
+	return db.Offset(offset).Limit(limit)
+}
+
+func (gormDB *gormDB) PaginateWithResult(query paginator.Query, options paginator.Options) Result {
+	db := gormDB.
+	
+	return db.Offset(offset).Limit(limit)
 }
